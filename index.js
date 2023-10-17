363855cd79843548b4513c8cdb739ac56cb6b329
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-10-17 17:47:30 +0200

    Add base to multiplier

---
+++ index.js
@@ -0,0 +1,3 @@
+import { Multiplication } from './src/model/multiplication.js'
+
+const multiplication = new Multiplication()
---
+++ src/model/multiplication.js
@@ -0,0 +1,16 @@
+
+export class Multiplication {
+    constructor() {
+        this.numberAnswer = document.querySelector('#numberAnswer')
+        this.answerForm = document.querySelector('#answerForm')
+        this.heading = document.querySelector('h1')
+
+        this.answerForm.addEventListener('submit', this.handleSubmit.bind(this))
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const inputValue = this.numberAnswer.value
+        this.heading.textContent = "Your Answer: " + inputValue
+    }
+}
