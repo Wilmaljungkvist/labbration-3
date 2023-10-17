1e919f4941b6c2e65f330c0e203f2ab967110f91
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-10-17 18:22:58 +0200

    Add simple math without slumpgenerator

--- src/multiplication.js
+++ src/multiplication.js
@@ -1,19 +1,35 @@
 import { NumberGenerator } from 'slumpgenerator'
+import { MathQuestion } from './mathQuestion'
 
 export class Multiplication {
 
     constructor() {
-        this.numberGenerator = new NumberGenerator
+        this.question = new MathQuestion()
+        this.numberGenerator = new NumberGenerator()
         this.numberAnswer = document.querySelector('#numberAnswer')
         this.answerForm = document.querySelector('#answerForm')
         this.heading = document.querySelector('h1')
+        this.p = document.querySelector('.correct')
 
         this.answerForm.addEventListener('submit', this.handleSubmit.bind(this))
+        this.generateNewQuestion()
     }
 
     handleSubmit(event) {
         event.preventDefault()
         const inputValue = this.numberAnswer.value
-        this.heading.textContent = this.numberGenerator.getRandomInt(Number.parseInt(inputValue), (Number.parseInt(inputValue) + 10))
+
+        if (parseInt(inputValue) === this.question.correctAnswer) {
+            this.p.textContent = 'Correct!'
+        } else {
+            this.p.textContent = 'Incorrect. Try again.'
+        }
+
+        this.generateNewQuestion()
+    }
+
+    generateNewQuestion() {
+        this.question.generateNewQuestion()
+        this.heading.textContent = this.question.question
     }
 }
