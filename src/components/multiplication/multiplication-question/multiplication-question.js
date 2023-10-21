679d9688c8fca5eb47284af99c0221f6f3e92870
Wilma Ljungkvist <wl222ek@student.lnu.se>
2023-10-21 14:36:15 +0200

    Make the code quality better in multiplication

--- src/components/multiplication/multiplication-question/multiplication-question.js
+++ src/components/multiplication/multiplication-question/multiplication-question.js
@@ -22,11 +22,13 @@ customElements.define('multiplication-question', class extends HTMLElement {
 
       this.numberGenerator = new NumberGenerator()
       this.arrayGenerator = new ArrayGenerator()
-      this.loadExternalCss('../../../public/css/styles.css')
+
+      this.loadExternalCss()
+
       this.initializeVariables()
   }
 
-  loadExternalCss(path) {
+  loadExternalCss() {
     const link = document.createElement('link')
         link.setAttribute('rel', 'stylesheet')
         link.setAttribute('href', '../../../public/css/styles.css')
@@ -51,11 +53,10 @@ customElements.define('multiplication-question', class extends HTMLElement {
   }
 
   initialize(table, rounds, high, low) {
-    this.totalRounds = Number.parseInt(rounds)
-    this.table = Number.parseInt(table)
-    this.high = Number.parseInt(high)
-    this.low = Number.parseInt(low)
-    this.currentRound = 0
+    this.totalRounds = Number.parseInt(rounds, 10)
+    this.table = Number.parseInt(table, 10)
+    this.high = Number.parseInt(high, 10)
+    this.low = Number.parseInt(low, 10)
     this.startRound()
 }
 
@@ -65,7 +66,12 @@ customElements.define('multiplication-question', class extends HTMLElement {
     if (this.currentRound < this.totalRounds) {
       this.generateNewQuestion(this.table)
     } else {
-      this.h1.textContent = 'Slut på frågor'
+      handleGameOver()
+    }
+  }
+
+  handleGameOver() {
+    this.h1.textContent = 'Slut på frågor'
       const restartButton = document.createElement('button')
       this.container.appendChild(restartButton)
       restartButton.textContent = 'Köra igen?'
@@ -74,67 +80,79 @@ customElements.define('multiplication-question', class extends HTMLElement {
       homeButton.textContent = 'Gå till hemskärm?'
 
       homeButton.addEventListener('click', () => {
-        const event = new CustomEvent('home-start', {
-          bubbles: true,
-          composed: true,
-        })
-        this.dispatchEvent(event)
+        this.dispatchHomeEvent()
       })
 
       restartButton.addEventListener('click', () => {
-        const event = new CustomEvent('multiply-start', {
-          bubbles: true,
-          composed: true,
-          })
-        this.dispatchEvent(event)
-        console.log('Disatches event!')
+        this.dispatchMultiplyStartEvent()
       })
 
       this.label.textContent = 'Bra jobbat! Vad vill du göra nu?'
       this.numberAnswer.remove()
       this.submit.remove()
-    }
   }
 
+  dispatchMultiplyStartEvent() {
+    const event = new CustomEvent('multiply-start', {
+      bubbles: true,
+      composed: true,
+      })
+      this.dispatchEvent(event)
+  }
+
+  dispatchHomeEvent() {
+    const event = new CustomEvent('home-start', {
+      bubbles: true,
+      composed: true,
+    })
+    this.dispatchEvent(event)
+  }
 
   generateNewQuestion(table) {
-      const num1 = Number.parseInt(table)
-      const num2 = this.numberGenerator.getRandomInt(this.low, (this.high + 1))
+    const num1 = Number.parseInt(table)
+    const num2 = this.numberGenerator.getRandomInt(this.low, (this.high + 1))
 
     this.correctAnswer = num1 * num2
     this.h1.textContent = `What is ${num1} x ${num2}?`
 }
 
+handleUserAnswer() {
+  const correctMessages = [
+    'Korrekt, bra jobbat!',
+    'De va rätt, snyggt!',
+    'Ding ding, ett poäng till dig', 
+    'Bra gissat!',
+    'Du har helt rätt!',
+    'Du har verkligen koll.',
+    'Mitt i prick!'
+  ]
+  const wrongMessages = [
+    'Fel!',
+    'Tyvärr så va det fel. ',
+    'Bra försök, men svaret är fel.',
+    'Fel! :(',
+    'Fel! :|'
+  ]
+  const indexCorrect = this.arrayGenerator.getRandomArrayIndex(correctMessages)
+  const indexWrong = this.arrayGenerator.getRandomArrayIndex(wrongMessages)
+  const userAnswer = parseInt(this.shadowRoot.querySelector('#numberAnswer').value)
+  if (userAnswer === this.correctAnswer) {
+    this.h1.textContent = correctMessages[indexCorrect]
+    this.scoreCount += 1
+  } else {
+    this.h1.textContent = wrongMessages[indexWrong]
+  }
+  this.updateScore()
+}
+
+updateScore() {
+  this.score.textContent = 'Poäng: ' + this.scoreCount + '/' + (this.currentRound + 1)
+}
+
 connectedCallback() {
     this.form.addEventListener('submit', (event) => {
       event.preventDefault()
-      const correctMessages = [
-        'Korrekt, bra jobbat!',
-        'De va rätt, snyggt!',
-        'Ding ding, ett poäng till dig', 
-        'Bra gissat!',
-        'Du har helt rätt!',
-        'Du har verkligen koll.',
-        'Mitt i prick!'
-      ]
-      const wrongMessages = [
-        'Fel!',
-        'Tyvärr så va det fel. ',
-        'Bra försök, men svaret är fel.',
-        'Fel! :(',
-        'Fel! :|'
-      ]
-      const indexCorrect = this.arrayGenerator.getRandomArrayIndex(correctMessages)
-      const indexWrong = this.arrayGenerator.getRandomArrayIndex(wrongMessages)
-      const userAnswer = parseInt(this.shadowRoot.querySelector('#numberAnswer').value)
-      if (userAnswer === this.correctAnswer) {
-        this.h1.textContent = correctMessages[indexCorrect]
-        this.scoreCount += 1
-        this.score.textContent = 'Poäng: ' + this.scoreCount + '/' + (this.currentRound + 1)
-      } else {
-        this.h1.textContent = wrongMessages[indexWrong]
-        this.score.textContent = 'Poäng: ' + this.scoreCount + '/' + (this.currentRound + 1)
-      }
+      this.handleUserAnswer()
 
       setTimeout(() => {
         this.currentRound += 1
